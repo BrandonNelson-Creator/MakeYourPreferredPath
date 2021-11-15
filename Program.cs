be99ea7cdc967f9d98a96c0790f4e51de41c56db
BrandonNelson-Creator <n.ellyson@hotmail.com>
2021-11-15 14:06:20 -0400

    Made a base to work from

--- Program.cs
+++ Program.cs
@@ -3,15 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace MakeYourPreferredPath
 {
     class Program
     {
+        static string player;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Make Your Preferred Path");
-            Console.ReadKey(true);
+           
+
+            WriteLine("Make Your Preferred Path");
+            ReadKey(true);
+            NamePlayer();
+            Story();
+            ReadKey(true);
         }
+
+        static void Story()
+        {
+            
+
+            string input = " ";
+            WriteLine("Which path will you take? A or B?");
+            input = ReadLine();
+            input = input.ToUpper();
+            if (input == "A")
+            {
+                WriteLine(player + " You've chosen path A!");
+                Beep();
+            }
+            if (input == "B")
+            {
+                WriteLine(player + " You've chosen path B!");
+                Beep();
+            }
+        }
+
+        static void NamePlayer()
+        {
+            WriteLine("Whats your name?");
+            player = ReadLine();
+
+            WriteLine("Great! " + player + ", Let's Begin.");
+
+        }
+
+
+        
     }
 }
